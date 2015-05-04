f0cb75f7e2e1339c222e945cc7418b1d51947dde
Linesh(Cylin) <linesh.simplicity@gmail.com>
2015-05-04 14:29:33 +0800

    [#6][Linesh] Refactor the test using the UserCreationMessage object.

--- service/src/main/java/com/thoughtworks/fam/web/UserCreationController.java
+++ service/src/main/java/com/thoughtworks/fam/web/UserCreationController.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.fam.web;
 
+import com.thoughtworks.fam.web.dto.UserCreationMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/admin")
 public class UserCreationController {
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String createAnUser(String username) {
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public UserCreationMessage createAnUser(String username) {
         if (username == null || username.equals("")) {
-            return "Please enter an username";
+            return new UserCreationMessage("failed", "Please enter an username");
         }
 
         if (username.equals("hello")) {
-            return "Invalid username in ThoughtWorks system.";
+            return new UserCreationMessage("failed", "Invalid username in ThoughtWorks system.");
         } else {
-            return "User has been succesfully created.";
+            return new UserCreationMessage("success", "User has been succesfully created.");
         }
     }
 }
--- service/src/test/java/com/thoughtworks/fam/web/UserCreationControllerTest.java
+++ service/src/test/java/com/thoughtworks/fam/web/UserCreationControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UserCreationControllerTest {
@@ -25,23 +27,26 @@ public class UserCreationControllerTest {
     }
 
     @Test
-    public void should_return_please_enter_an_username_when_given_an_empty_username() throws Exception {
-        this.mockMvc.perform(post("/admin", ""))
+    public void should_return_create_successful_message_when_given_a_valid_username() throws Exception {
+        this.mockMvc.perform(post("/admin/create").param("username", "jtao"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Please enter an username"));
+                .andExpect(jsonPath("creationStatus").value("success"))
+                .andExpect(jsonPath("message").value("User has been succesfully created."));
+    }
+
+    @Test
+    public void should_return_please_enter_an_username_when_given_an_empty_username() throws Exception {
+        this.mockMvc.perform(post("/admin/create", ""))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("creationStatus").value("failed"))
+                .andExpect(jsonPath("message").value("Please enter an username"));
     }
 
     @Test
     public void should_return_invalid_username_when_given_one() throws Exception {
-        this.mockMvc.perform(post("/admin").param("username", "hello"))
+        this.mockMvc.perform(post("/admin/create").param("username", "hello"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Invalid username in ThoughtWorks system."));
-    }
-
-    @Test
-    public void should_return_create_successful_message_when_given_a_valid_username() throws Exception {
-        this.mockMvc.perform(post("/admin").param("username", "jtao"))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("User has been succesfully created."));
+                .andExpect(jsonPath("creationStatus").value("failed"))
+                .andExpect(jsonPath("message").value("Invalid username in ThoughtWorks system."));
     }
 }
