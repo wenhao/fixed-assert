e6ea71a1e156cf7ebc24d40b22b22417fe659928
Shuwen Li <shuwenli89@126.com>
2015-05-09 12:57:06 +0800

    [#7][Zeng&Shuwen&Sun] add test for login.

--- client/src/services/__test__/user.spec.js
+++ client/src/services/__test__/user.spec.js
@@ -4,15 +4,40 @@ import user from '../user'
 
 describe('User Services', function() {
   it('should be able to login', function(done) {
-    user.login()
-      .then(function(data) {
-        data.should.be.equal('success')
-        done()
-      })
-      .catch(function(err) {
-        done(err)
-      })
+    user.login({
+        username: "admin",
+        password: "pw"
+    })
+    .then(function(data) {
+      data.username.should.be.equal('admin')
+      done()
+    })
+    .catch(done)
   });
+  it('should not be able to login when password is incorrect', function(done) {
+    user.login({
+      username: "admin",
+      password: "123"
+    })
+    .then(null, function(err) {
+      err.errorMessage.should.be.equal('The password is not correct, please input again.')
+      err.httpStatus.should.be.equal(401)
+      done()
+    })
+    .catch(done)
+  })
+  it('should not be able to login when is not exist', function(done) {
+    user.login({
+      username: "123",
+      password: "123"
+    })
+    .then(null, function(err) {
+      err.errorMessage.should.be.equal('The user is not exist.')
+      err.httpStatus.should.be.equal(401)
+        done()
+    })
+    .catch(done)
+  })
   it('should be able to create user', function(done) {
     user.create({
       name: 'LSQ',
--- client/src/services/mock-user.js
+++ client/src/services/mock-user.js
@@ -2,8 +2,31 @@ export default [
   {
     pattern: 'http://localhost:8080/auth/(login|logout)',
     // callback that returns the data
-    fixtures: function () {
-      return 'success'
+    fixtures: function (data) {
+      if (data) {
+        if (data.username === 'admin') {
+          if (data.password === 'pw') {
+            //login successful
+            return {username: 'admin'}
+          } else {
+            //password is not correct
+            throw new function() {
+              this.code = ''
+              this.errorMessage = 'The password is not correct, please input again.'
+              this.httpStatus = 401
+            };
+            return
+          }
+        } else {
+          //use is not found
+          throw new function() {
+            this.code = ''
+            this.errorMessage = 'The user is not exist.'
+            this.httpStatus = 401
+          };
+        }
+      }
+      throw new Error('Request not found!')
     },
     // `match`: result of the resolution of the regular expression
     // `data`: data returns by `fixtures` attribute
