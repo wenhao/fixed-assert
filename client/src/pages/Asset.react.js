fb6a8409a59a5c318a3b599a0ea9d9050f17eed0
Waterstrong <sqlin@thoughtworks.com>
2015-05-12 18:15:18 +0800

    [#1][Waterstrong] - update User Assets page.

--- client/src/pages/Asset.react.js
+++ client/src/pages/Asset.react.js
@@ -2,88 +2,98 @@
 
 import React from 'react'
 import {
-  RaisedButton,
-  FontIcon,
-  Paper,
-  Tabs,Tab
-} from 'material-ui'
+    RaisedButton,
+    FontIcon,
+    Paper,
+    Tabs,Tab
+    } from 'material-ui'
 import {
-  State
-} from 'react-router'
+    State
+    } from 'react-router'
 
-//import assetApi from '../services/asset'
-import tempAssetApis from '../services/temp-asset.js'
+import assetApi from '../services/asset'
 
 var Asset = React.createClass({
 
-  mixins: [State],
+    mixins: [State],
 
-  getInitialState() {
-    return {
-      title: 'User Assets',
-      assets: []
-    }
-  },
-  componentDidMount() {
-    this._getAssets()
-  },
-  render() {
-    return (
-      <Paper zDepth={1} className="page-asset-list">
-        <RaisedButton secondary={true} >
-          <FontIcon className="muidocs-icon-custom-github example-button-icon"/>
-          <span className="mui-raised-button-label example-icon-button-label">Get Assets</span>
-        </RaisedButton>
-        <Tabs>
-          <Tab label="My Assets" onActive={this._getAssets}>
-            <div className="tab-template-container asset-group">
-              <ul className="asset-header">
+    getInitialState() {
+        return {
+            title: 'User Assets',
+            assets: []
+        }
+    },
+    componentDidMount() {
+        this._getAssets()
+    },
+    render() {
+        return (
+            <Paper zDepth={1} className="page-asset-list">
+                <RaisedButton secondary={true} >
+                    <FontIcon className="muidocs-icon-custom-github example-button-icon"/>
+                    <span className="mui-raised-button-label example-icon-button-label">Get Assets</span>
+                </RaisedButton>
+                <Tabs>
+                    <Tab label="My Assets" onActive={this._getAssets}>
+                        <div className="tab-template-container asset-group">
+                            {this._renderAssetHeader()}
+                            <ul className="asset-list">
+                                {this._renderAssets()}
+                            </ul>
+                        </div>
+                    </Tab>
+                    <Tab label="Others Assets">
+                        <div className="tab-template-container asset-group">
+                            {this._renderAssetHeader()}
+                            <ul className="asset-list">
+
+                            </ul>
+                        </div>
+                    </Tab>
+                </Tabs>
+            </Paper>
+        );
+    },
+    _getAssets(userId) {
+        assetApi.getUserAssets(userId).then(this.onAssetsLoad, this.onAssetsLoadFailed)
+    },
+    onAssetsLoad(result) {
+        console.log(result);
+        this.setState({
+            assets: result.body
+        })
+    },
+    onAssetsLoadFailed(error) {
+        //
+    },
+    _renderAssetHeader() {
+        return (
+            <ul className="asset-header">
                 <li className="asset-attribute">Asset Name</li>
                 <li className="asset-attribute">Asset Number</li>
                 <li className="asset-attribute">Assigned Date</li>
                 <li className="asset-attribute">Asset Type</li>
                 <li className="asset-attribute"></li>
-              </ul>
-              <ul className="asset-list">
-                {this._renderAssets()}
-              </ul>
-            </div>
-          </Tab>
-          <Tab label="Others Assets">
-            <div className="tab-template-container"></div>
-          </Tab>
-        </Tabs>
-      </Paper>
-    );
-  },
-  _getAssets(userId) {
-     //assetApi.getUserAssets(userId).then(this.onAssetsLoad, this.onAssetsLoadFailed)
-    tempAssetApis.getUserAssets(userId).then(this.onAssetsLoad, this.onAssetsLoadFailed)
-  },
-  onAssetsLoad(result) {
-  console.log(result);
-    this.setState({
-      assets: result.body
-    })
-  },
-  onAssetsLoadFailed(error) {
-    //
-  },
-  _renderAssets() {
-    return this.state.assets.map(function(result) {
-      return (
-        <li className="asset-item">
-          <ul>
-            <li className="asset-attribute">{result.assetName}</li>
-            <li className="asset-attribute">{result.assetNumber}</li>
-            <li className="asset-attribute">{result.assignedDate}</li>
-            <li className="asset-attribute">{result.assetType}</li>
-            <li className="asset-attribute"><RaisedButton label="view" secondary={true} /></li>
-          </ul>
-        </li>
-      )
-    })
-  }
-})
+            </ul>
+        )
+    },
+    _renderAssets() {
+        return this.state.assets.map(function (result) {
+            return (
+                <li className="asset-item">
+                    <ul>
+                        <li className="asset-attribute">{result.assetName}</li>
+                        <li className="asset-attribute">{result.assetNumber}</li>
+                        <li className="asset-attribute">{result.assignedDate}</li>
+                        <li className="asset-attribute">{result.assetType}</li>
+                        <li className="asset-attribute">
+                            <RaisedButton label="view" secondary={true} />
+                        </li>
+                    </ul>
+                </li>
+            )
+        })
+    }
+});
 
 export default Asset
