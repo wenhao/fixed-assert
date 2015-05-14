4a06203f932e2990694d89832740f6dfbeb42d62
Shuwen Li <shuwenli89@126.com>
2015-05-14 16:59:43 +0800

    [#7][Shuwen&Sun] - Add login controller.

--- service/src/test/java/com/thoughtworks/fam/resource/TempUserControllerTest.java
+++ service/src/test/java/com/thoughtworks/fam/resource/TempUserControllerTest.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.fam.resource;
 
 import com.thoughtworks.fam.resource.domain.TempAsset;
-import com.thoughtworks.fam.resource.domain.User;
 import com.thoughtworks.fam.service.TempUserService;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,11 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+
 import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -36,6 +35,7 @@ public class TempUserControllerTest
         MockitoAnnotations.initMocks(this);
 
         mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
     }
 
     @Test
@@ -56,8 +56,6 @@ public class TempUserControllerTest
 
     @Test
     public void should_be_able_to_create_user() throws Exception {
-        given(userService.createUser(any(User.class))).willReturn(true);
-
         mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON)
                 .content("{\"account\": \"test\", \"password\": \"123456\"}"))
                 .andExpect(status().isCreated());
@@ -65,8 +63,6 @@ public class TempUserControllerTest
 
     @Test
     public void should_get_conflict_status_code_when_account_exist() throws Exception {
-        given(userService.createUser(any(User.class))).willReturn(false);
-
         mockMvc.perform(post("/users")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"account\": \"twer\", \"password\": \"123456\"}"))
