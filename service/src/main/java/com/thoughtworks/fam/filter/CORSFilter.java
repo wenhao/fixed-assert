9f00a7d3666db8f80e2deab17364e906b97a85b3
Hao Wen <wenhao@126.com>
2015-05-16 16:03:19 +0800

    [TECH][Hao Wen] - add docs.

--- service/src/main/java/com/thoughtworks/fam/filter/CORSFilter.java
+++ service/src/main/java/com/thoughtworks/fam/filter/CORSFilter.java
@@ -17,7 +17,8 @@ public class CORSFilter implements Filter
 {
 
     // CHECKSTYLE:OFF RedundantThrowsCheck
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException
     {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
