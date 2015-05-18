df25cf43c06ad0fecf4eb80c38a09b3f67076d86
wrongkey <18280480654@163.com>
2015-05-18 16:16:38 +0800

    [TECH][wrongkey] - add application yaml.

--- service/src/main/java/com/thoughtworks/fam/filter/CORSFilter.java
+++ service/src/main/java/com/thoughtworks/fam/filter/CORSFilter.java
@@ -10,19 +10,23 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CORSFilter implements Filter
 {
+    @Value("${Access-Control-Allow-Origin}")
+    private String allowOrigin;
 
     // CHECKSTYLE:OFF RedundantThrowsCheck
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException
     {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        allowOrigin = "http://localhost:3000";
+        response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, HEAD, PUT, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "origin, content-type, accept, x-requested-with");
