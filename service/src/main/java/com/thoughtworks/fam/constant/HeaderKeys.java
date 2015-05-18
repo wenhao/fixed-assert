ef18c5497f3ddf698145b92968249b312959f46f
WrongKey <18280480654@163.com>
2015-05-18 18:22:32 +0800

    [TECH][wrongkey&waterstrong] - use yaml config file instead of hard code.

---
+++ service/src/main/java/com/thoughtworks/fam/constant/HeaderKeys.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.fam.constant;
+
+public class HeaderKeys
+{
+    public static final String ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    public static final String ALLOW_METHODS = "Access-Control-Allow-Methods";
+    public static final String MAX_AGE = "3600";
+    public static final String ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    public static final String CONTENT_TYPE = "Content-Type";
+}
--- service/src/main/java/com/thoughtworks/fam/filter/CORSFilter.java
+++ service/src/main/java/com/thoughtworks/fam/filter/CORSFilter.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.fam.filter;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,28 +8,30 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import com.thoughtworks.fam.constant.HeaderKeys;
 
 @Component
+@ConfigurationProperties(prefix = "cors")
 public class CORSFilter implements Filter
 {
-    @Value("${Access-Control-Allow-Origin}")
     private String allowOrigin;
+    private String allowMethods;
+    private String contentType;
+    private String allowHeaders;
+    private String maxAge;
 
     // CHECKSTYLE:OFF RedundantThrowsCheck
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException
     {
         HttpServletResponse response = (HttpServletResponse) res;
-        allowOrigin = "http://localhost:3000";
-        response.setHeader("Access-Control-Allow-Origin", allowOrigin);
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, HEAD, PUT, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-                "origin, content-type, accept, x-requested-with");
-        response.setHeader("Content-Type", "application/json");
+        response.setHeader(HeaderKeys.ALLOW_ORIGIN, allowOrigin);
+        response.setHeader(HeaderKeys.ALLOW_METHODS, allowMethods);
+        response.setHeader(HeaderKeys.MAX_AGE, maxAge);
+        response.setHeader(HeaderKeys.ALLOW_HEADERS, allowHeaders);
+        response.setHeader(HeaderKeys.CONTENT_TYPE, contentType);
         chain.doFilter(req, res);
     }
     // CHECKSTYLE:ON
@@ -43,4 +44,28 @@ public class CORSFilter implements Filter
     {
     }
 
+    public void setAllowOrigin(String allowOrigin)
+    {
+        this.allowOrigin = allowOrigin;
+    }
+
+    public void setAllowMethods(String allowMethods)
+    {
+        this.allowMethods = allowMethods;
+    }
+
+    public void setContentType(String contentType)
+    {
+        this.contentType = contentType;
+    }
+
+    public void setAllowHeaders(String allowHeaders)
+    {
+        this.allowHeaders = allowHeaders;
+    }
+
+    public void setMaxAge(String maxAge)
+    {
+        this.maxAge = maxAge;
+    }
 }
