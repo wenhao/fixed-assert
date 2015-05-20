f074f5f882543de6a48feb5be8156becbed39651
lwzhang <lwzhang@thoughtworks.com>
2015-05-20 23:58:58 +0800

    [#14][Leewin] - Made user and asset as entity

--- service/src/main/java/com/thoughtworks/fam/domain/Asset.java
+++ service/src/main/java/com/thoughtworks/fam/domain/Asset.java
@@ -1,10 +1,31 @@
 package com.thoughtworks.fam.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TABLE_ASSETS")
 public class Asset
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ASSET_ID")
+    private long id;
+
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Column(name = "ASSET_NAME")
     private String assetName;
+
+    @Column(name = "ASSET_NUMBER")
     private String assetNumber;
+
+    @Column(name = "ASSIGNED_DATE")
+    @Temporal(TemporalType.DATE)
     private String assignedDate;
+
+    @Column(name = "ASSET_TYPE")
     private String assetType;
 
     public Asset(String assetName, String assetNumber, String assignedDate, String assetType)
@@ -35,5 +56,15 @@ public class Asset
     public String getAssignedDate()
     {
         return assignedDate;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public User getUser()
+    {
+        return user;
     }
 }
--- service/src/main/java/com/thoughtworks/fam/domain/User.java
+++ service/src/main/java/com/thoughtworks/fam/domain/User.java
@@ -1,9 +1,25 @@
 package com.thoughtworks.fam.domain;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "TABLE_USER")
 public class User
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "USER_ID")
+    private long id;
+
+    @Column(name = "USER_ACCOUNT")
     private String account;
+
+    @Column(name = "USER_PASSWORD")
     private String password;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Asset> assets;
 
     public User()
     {
@@ -33,5 +49,15 @@ public class User
     public String getPassword()
     {
         return password;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public List<Asset> getAssets()
+    {
+        return assets;
     }
 }
